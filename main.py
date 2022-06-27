d70b8a45371f19c7b1701ab9f50dcb780c2aad91
Wade Coghlan <wcoghlan@live.com.au>
2022-06-27 18:14:02 +1000

    Added Env File

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 login_manager = LoginManager()
